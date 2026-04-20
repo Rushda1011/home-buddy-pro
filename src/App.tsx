282a55344c1f957604a1e1f4497d83e183f973dd
Rushda <rushda@exampe.com>
2026-04-21 02:30:06 +0530

    Fix routing for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/home-buddy-pro/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/dashboard" element={<Dashboard />} />
